54ed0ecd650e261cb127173a3ff66933bf108c8d
Jason Sweatt <jasonsweatt@shipcarousel.com>
2022-09-26 08:59:54 -0400

    changed case

--- sonar-scanner-begin/index.js
+++ sonar-scanner-begin/index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
-const utils = require('@ShipCarousel/carousel-actions-lib');
+const utils = require('@shipcarousel/carousel-actions-lib');
 
 async function run()
 {
